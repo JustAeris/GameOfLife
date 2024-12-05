e75338c8d1c69fc99235bc586b8f84e255e7018a
JustAeris <aeris.github@gmail.com>
2024-12-05 10:42:41 +0100

    Added a cell interface

---
+++ lib/GameOfLife/Game/BaseCell.h
@@ -0,0 +1,27 @@
+#ifndef BASECELL_H
+#define BASECELL_H
+
+namespace GameOfLife::Game {
+    /**
+     * Basic cell interface, it can be derived to add more complex behavior
+     */
+    class BaseCell {
+    private:
+        bool alive;
+    public:
+        BaseCell();
+        explicit BaseCell(bool alive);
+        virtual ~BaseCell() = default;
+
+        [[nodiscard]] virtual bool isAlive() const = 0;
+        virtual void setAlive(bool alive) = 0;
+        [[nodiscard]] virtual bool willBeAlive(int aliveNeighbours) const = 0;
+
+        virtual void print() const = 0;
+
+        virtual BaseCell& operator=(bool alive) = 0;
+        virtual bool operator==(const BaseCell &cell) const = 0;
+    };
+}
+
+#endif //BASECELL_H
