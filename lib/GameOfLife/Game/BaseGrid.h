d646117492b2ead9a010246f30f1cacccf82e54d
JustAeris <aeris.github@gmail.com>
2024-12-05 15:10:24 +0100

    Rewrote the grid interface

--- lib/GameOfLife/Game/BaseGrid.h
+++ lib/GameOfLife/Game/BaseGrid.h
@@ -1,13 +1,15 @@
 #ifndef BASEGRID_H
 #define BASEGRID_H
-#include "Cell.h"
+#include <string>
+#include "File/IWritable.h"
 
 
 namespace GameOfLife::Game {
     /**
      * Base interface for a grid
      */
-    class BaseGrid {
+    template<typename TGrid>
+    class BaseGrid<TGrid> : public File::IWritable {
     public:
         BaseGrid() = default;
         virtual ~BaseGrid() = default;
@@ -15,18 +17,20 @@ namespace GameOfLife::Game {
         virtual void setAlive(int row, int col, bool alive) = 0;
         [[nodiscard]] virtual bool isAlive(int row, int col) const = 0;
         [[nodiscard]] virtual int countNeighbors(int row, int col, bool wrap) const = 0;
-        virtual void toggle(int row, int col) = 0;
 
         virtual void step() = 0;
         virtual void step(bool wrap) = 0;
-        virtual void randomize() = 0;
+        virtual void randomize(float aliveProbability) = 0;
         virtual void clear() = 0;
 
         virtual void print() const = 0;
         virtual void print(int fromRow, int fromCol, int toRow, int toCol) const = 0;
 
-        [[nodiscard]] virtual std::vector<std::vector<Cell>> getCells() const = 0;
-        [[nodiscard]] virtual std::vector<std::vector<bool>> toBooleanGrid() const = 0;
+        [[nodiscard]] virtual int getRows() const = 0;
+        [[nodiscard]] virtual int getCols() const = 0;
+        [[nodiscard]] virtual TGrid getCells() const = 0;
+
+        [[nodiscard]] std::string getText() const override = 0;
     };
 }
 
